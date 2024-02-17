9d97052b7ee3a6af80ba1c0076d3c5d827ff346e
vishalkhillare <113585079+vishalkhillare@users.noreply.github.com>
2024-02-17 14:45:10 +0530

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -2,6 +2,19 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('GET /', () => {
+  let server;
+
+  beforeAll((done) => {
+    server = app.listen(3000, () => {
+      console.log('Server started');
+      done();
+    });
+  });
+
+  afterAll((done) => {
+    server.close(done);
+  });
+
   it('responds with "Hello World!"', async () => {
     const response = await request(app).get('/');
     expect(response.text).toBe('Hello World!');
